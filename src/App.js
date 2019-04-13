396971002a86b7f251623f34d7c20e793805991f
shreyas1307 <shreyas.bba@gmail.com>
2019-04-13 15:51:47 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    movieTitle: "",
+    movieYear: "",
+    plotRequest: "",
+    movieDetails: {
+      Title: "",
+      Year: "",
+      Rated: "",
+      Released: "",
+      RunTime: "",
+      poster: "",
+      plot: ""
+    }
+  };
+
+  Getdata = () => {
+    console.log("clicked");
+    fetch(
+      `http://www.omdbapi.com/?t=${this.state.movieTitle}&y=${
+        this.state.movieYear
+      }&plot=${this.state.plotRequest}&apikey=e2d95701`
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          movieDetails: {
+            Title: data.Title,
+            Year: data.Year,
+            Rated: data.Rated,
+            Released: data.Released,
+            RunTime: data.Runtime,
+            poster: data.Poster,
+            plot: data.Plot
+          }
+        });
+      });
+  };
+
+  movieHandler = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="input">
+          <h1> Look up your Favorite Movie!</h1>
+          <input
+            type="text"
+            name="movieTitle"
+            value={this.state.movieTitle}
+            placeholder="Name"
+            onChange={this.movieHandler}
+          />
+          <input
+            type="text"
+            name="movieYear"
+            value={this.state.movieYear}
+            placeholder="Year"
+            onChange={this.movieHandler}
+          />
+          <select name="plotRequest" onChange={this.movieHandler}>
+            <option />
+            <option value="">Short Plot</option>
+            <option value="full">Full Plot</option>
+          </select>
+          <button onClick={this.Getdata}> Get Info!</button>
+          <br />
+          <br />
+          <span>Powered by OMDb!</span>
+        </div>
+
+        <div className="movie">
+          <div className="movie-poster">
+            <img
+              src={this.state.movieDetails.poster}
+              alt={this.state.movieDetails.Title}
+            />
+          </div>
+
+          <div className="movie-details">
+            <h3>Movie:</h3>
+            <p>{this.state.movieDetails.Title}</p>
+            <h3>Year:</h3>
+            <p>{this.state.movieDetails.Year}</p>
+            <h3>Rated:</h3>
+            <p>{this.state.movieDetails.Rated}</p>
+            <h3>Released:</h3>
+            <p>{this.state.movieDetails.Released}</p>
+            <h3>Runtime:</h3>
+            <p>{this.state.movieDetails.RunTime}</p>
+          </div>
+
+          <div className="movie-plot">
+            <p>{this.state.movieDetails.plot}</p>
+          </div>
+        </div>
       </div>
     );
   }
