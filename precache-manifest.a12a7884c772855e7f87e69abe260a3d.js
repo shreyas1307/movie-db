7c95086bee07bd7e4fe11f71451fdf5d35a96f37
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-06-06 23:54:32 +0530

    Updates

---
+++ precache-manifest.a12a7884c772855e7f87e69abe260a3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e817e6dbc7750d5812af",
+    "url": "/movie-db/static/css/main.9014311c.chunk.css"
+  },
+  {
+    "revision": "e817e6dbc7750d5812af",
+    "url": "/movie-db/static/js/main.da8c81d0.chunk.js"
+  },
+  {
+    "revision": "6bb80631b382c7965b33",
+    "url": "/movie-db/static/js/runtime~main.5114c00d.js"
+  },
+  {
+    "revision": "3f19e69d1c6a492396e7",
+    "url": "/movie-db/static/js/2.a61b71c8.chunk.js"
+  },
+  {
+    "revision": "558f9a158ee6ee01f0769791beaae081",
+    "url": "/movie-db/index.html"
+  }
+];
